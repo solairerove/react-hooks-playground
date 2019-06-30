8add085dee7de6bd3af1d5b29c8efaac7bf932ff
Mikita Kryvitski <solairerove@gmail.com>
2019-06-30 14:24:39 +0300

    feature: simple app with component level state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
-const App = () => {
-  return <div className="ui container">App</div>;
-};
+class App extends React.Component {
+  state = { resource: 'posts' };
+
+  onButtonClick = resource => {
+    this.setState({ resource: resource });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          <button
+            onClick={() => this.onButtonClick('posts')}
+            className="ui button primary"
+          >
+            Posts
+          </button>
+          <button
+            onClick={() => this.onButtonClick('todos')}
+            className="ui button primary"
+          >
+            Todos
+          </button>
+        </div>
+        {this.state.resource}
+      </div>
+    );
+  }
+}
 
 export default App;
