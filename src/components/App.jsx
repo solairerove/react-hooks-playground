48c2214cd645b36529092fd2c554270ab1952591
Mikita Kryvitski <solairerove@gmail.com>
2019-06-30 14:56:58 +0300

    feature: add resource list class based component - install axios dependency

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ResourceList from './ResourceList';
 
 const App = () => {
   const [resource, setResource] = useState('posts');
@@ -23,7 +24,7 @@ const App = () => {
           Todos
         </button>
       </div>
-      {resource}
+      <ResourceList resource={resource} />
     </div>
   );
 };
